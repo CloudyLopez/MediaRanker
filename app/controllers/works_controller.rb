f450b7c02208b5542376a157b8413f51250bfb2e
cloudy lopez <cloudylopez@gmail.com>
2019-04-26 15:23:40 -0700

    got the route to work properly and save the new work we've created

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -7,19 +7,24 @@ class WorksController < ApplicationController
 
   end
 
-  def def new
-    @work = Work.new
-  end
-
   def show
     @work = Work.find_by(id: params[:id])
   end
 
+  def new
+    @work = Work.new
+  end
 
-
-end
+  def create
+    @work = Work.new(work_params)
+    @work.save 
+    redirect_to work_path(@work.id)
+  end
 
 private 
-def work_params
-  return params.require(:work).permit(:category, :title, :publication_year, :description, :creator)
+
+  def work_params
+    return params.require(:work).permit(:category, :title, :publication_year, :description, :creator)
+  end
+
 end
