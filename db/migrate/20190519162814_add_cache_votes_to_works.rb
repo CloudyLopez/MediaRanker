ce067a8c219dc2fee650903c2a44afdda288004f
Cloudy Lopez <cloudylopez@gmail.com>
2019-05-19 09:56:10 -0700

    created new migrations for acts as votable and the voter cache. updated the index and show works.

---
+++ db/migrate/20190519162814_add_cache_votes_to_works.rb
@@ -0,0 +1,10 @@
+class AddCacheVotesToWorks < ActiveRecord::Migration[5.2]
+  def change
+    add_column :works, :cached_votes_total, :integer, default: 0
+    add_index :works, :cached_votes_total
+    add_column :works, :cached_votes_up, :integer, default: 0
+    add_column :works, :cached_votes_down, :integer, default: 0
+    add_index  :works, :cached_votes_up
+    add_index  :works, :cached_votes_down
+  end
+end
